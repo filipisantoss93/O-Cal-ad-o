0e7b517aee0e4e8e82a0cef01f0444d5715e2164
filipisantoss93 <filipi.199320@gmail.com>
2026-09-18 10:21:26 -0300

    Libera normalizadores para importação interna

---
+++ supabase/migrations/20260918131403_allow_service_role_network_import_normalizers.sql
@@ -0,0 +1,3 @@
+grant usage on schema private to service_role;
+grant execute on function private.normalize_business_text(text) to service_role;
+grant execute on function private.normalize_postal_code(text) to service_role;
